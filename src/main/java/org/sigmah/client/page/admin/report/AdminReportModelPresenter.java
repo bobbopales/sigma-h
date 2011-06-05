f386246a039622dc6f59f531bc1f20016cfcfe45
olivier.sarrat <olivier.sarrat@46f870f5-83fa-7f44-208d-45d3d0f2e120>
2011-06-05 13:57:02 +0000

    Tag for version 1.0-rc1

---
+++ src/main/java/org/sigmah/client/page/admin/report/AdminReportModelPresenter.java
@@ -0,0 +1,108 @@
+package org.sigmah.client.page.admin.report;
+
+import org.sigmah.client.dispatch.Dispatcher;
+import org.sigmah.client.dispatch.monitor.MaskingAsyncMonitor;
+import org.sigmah.client.page.admin.AdminPageState;
+import org.sigmah.client.page.admin.AdminUtil;
+import org.sigmah.client.page.admin.model.AdminModelSubPresenter;
+import org.sigmah.shared.command.GetReportModels;
+import org.sigmah.shared.command.result.ReportModelsListResult;
+import org.sigmah.shared.dto.ProjectModelDTO;
+import org.sigmah.shared.dto.report.ReportModelDTO;
+
+import com.allen_sauer.gwt.log.client.Log;
+import com.extjs.gxt.ui.client.store.ListStore;
+import com.extjs.gxt.ui.client.widget.Component;
+import com.extjs.gxt.ui.client.widget.ContentPanel;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+public class AdminReportModelPresenter implements AdminModelSubPresenter {
+	
+	private static boolean alert = false;
+
+	private final View view;
+	private final Dispatcher dispatcher;
+	private ProjectModelDTO projectModel;
+	
+	public static abstract class View extends ContentPanel {
+		public abstract ListStore<ReportModelDTO> getReportModelsStore();
+		public abstract Component getMainPanel();
+		public abstract MaskingAsyncMonitor getReportModelsLoadingMonitor();
+	}
+	
+	public AdminReportModelPresenter(Dispatcher dispatcher){
+		this.view = new AdminReportModelView(dispatcher);
+		this.dispatcher = dispatcher;
+	}
+
+	@Override
+	public Component getView() {
+		dispatcher.execute(new GetReportModels(), 
+        		null,
+        		new AsyncCallback<ReportModelsListResult>() {
+
+				@Override
+	            public void onFailure(Throwable arg0) {
+	                AdminUtil.alertPbmData(alert);
+	            }
+	
+	            @Override
+	            public void onSuccess(ReportModelsListResult result) {
+	            	Log.debug("id for report " + (ReportModelDTO) result.getList().get(0));
+	            	view.getReportModelsStore().removeAll();
+	            	view.getReportModelsStore().clearFilters();
+	                if (result.getList() == null || result.getList().isEmpty()) {
+	                	 AdminUtil.alertPbmData(alert);
+	                    return;
+	                }
+	                view.getReportModelsStore().add(result.getList());
+	                view.getReportModelsStore().commitChanges();
+	            }		
+		});
+		
+		return view.getMainPanel();
+	}
+
+	@Override
+	public void discardView() {
+	}
+
+	@Override
+	public void viewDidAppear() {
+	}
+
+	@Override
+	public void setCurrentState(AdminPageState currentState) {
+	}
+
+	@Override
+	public void setModel(Object model) {
+		projectModel = (ProjectModelDTO)model;
+	}
+
+	@Override
+	public Object getModel() {
+		return projectModel;
+	}
+	
+	public static void refreshReportModelsPanel(Dispatcher dispatcher, final View view){
+		dispatcher.execute(new GetReportModels(), 
+				view.getReportModelsLoadingMonitor(),
+        		new AsyncCallback<ReportModelsListResult>() {
+        	@Override
+            public void onFailure(Throwable arg0) {
+        		AdminUtil.alertPbmData(alert);
+            }
+
+            @Override
+            public void onSuccess(ReportModelsListResult result) {
+            	if (result.getList() != null && !result.getList().isEmpty()) {
+            		view.getReportModelsStore().removeAll();
+            		view.getReportModelsStore().add(result.getList());
+                	view.getReportModelsStore().commitChanges();
+            	}
+            	
+            }
+        });
+	}
+}
---
+++ src/main/java/org/sigmah/server/endpoint/gwtrpc/handler/GetReportModelsHandler.java
@@ -0,0 +1,72 @@
+/*
+ * All Sigmah code is released under the GNU General Public License v3
+ * See COPYRIGHT.txt and LICENSE.txt.
+ */
+
+package org.sigmah.server.endpoint.gwtrpc.handler;
+
+import com.google.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Query;
+import javax.persistence.EntityManager;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.dozer.Mapper;
+import org.sigmah.shared.command.GetReportModels;
+import org.sigmah.shared.command.handler.CommandHandler;
+import org.sigmah.shared.command.result.CommandResult;
+import org.sigmah.shared.command.result.ReportModelsListResult;
+import org.sigmah.shared.domain.User;
+import org.sigmah.shared.domain.report.ProjectReportModel;
+import org.sigmah.shared.domain.report.ProjectReportModelSection;
+import org.sigmah.shared.dto.report.ProjectReportModelSectionDTO;
+import org.sigmah.shared.dto.report.ReportModelDTO;
+import org.sigmah.shared.exception.CommandException;
+
+/**
+ *
+ * @author nrebiai
+ */
+public class GetReportModelsHandler implements CommandHandler<GetReportModels> {
+    private EntityManager em;
+    private final Mapper mapper;
+
+    private static final Log log = LogFactory.getLog(GetReportModelsHandler.class);
+    
+    @Inject
+    public GetReportModelsHandler(EntityManager em, Mapper mapper) {
+        this.em = em;
+        this.mapper = mapper;
+    }
+
+    @Override
+    public CommandResult execute(GetReportModels cmd, User user) throws CommandException {
+        final ArrayList<ReportModelDTO> reports = new ArrayList<ReportModelDTO>();
+
+        final Query query = em.createQuery("SELECT r FROM ProjectReportModel r WHERE r.organization.id = :orgid  ORDER BY r.id");
+        query.setParameter("orgid", user.getOrganization().getId());
+        
+        @SuppressWarnings("unchecked")
+		final List<ProjectReportModel> models = query.getResultList();
+        for(final ProjectReportModel model : models){
+        	ReportModelDTO reportModel = mapper.map(model, ReportModelDTO.class);
+        	reportModel.setId(model.getId());//Set the id property for the DTO model
+        	List<ProjectReportModelSectionDTO> sections = new ArrayList<ProjectReportModelSectionDTO>();
+        	for(ProjectReportModelSection section : model.getSections()){
+        		ProjectReportModelSectionDTO sectionDTO = new ProjectReportModelSectionDTO();
+        		sectionDTO = mapper.map(section, ProjectReportModelSectionDTO.class);
+        		sections.add(sectionDTO);
+        	}       	
+			reportModel.setSectionsDTO(sections);
+        	reports.add(reportModel);
+        }
+            
+
+        ReportModelsListResult result = new ReportModelsListResult(reports); 
+        log.debug("Size of report models found : " + result.getList().size());
+        
+        return result;
+    }
+
+}
