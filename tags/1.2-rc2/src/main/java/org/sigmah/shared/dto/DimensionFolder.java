e0f9b724f1837ca108f63978e7dac5db8011777e
tom.miette@gmail.com <tom.miette@gmail.com@46f870f5-83fa-7f44-208d-45d3d0f2e120>
2013-06-12 12:46:42 +0000

    Tag for version 1.2, release candidate 2.

---
+++ tags/1.2-rc2/src/main/java/org/sigmah/shared/dto/DimensionFolder.java
@@ -0,0 +1,46 @@
+package org.sigmah.shared.dto;
+
+import com.extjs.gxt.ui.client.data.BaseModelData;
+import org.sigmah.shared.report.model.DimensionType;
+
+public class DimensionFolder extends BaseModelData {
+	
+	private static final long serialVersionUID = 9212781626829843237L;
+    private DimensionType type;
+	private int depth;
+	private int id;
+	
+    public DimensionFolder(String name, DimensionType type, int depth, int id) {
+    	this.type = type;
+    	this.depth = depth;
+    	this.id = id;
+    	set("caption", name);
+		set("id", "folder_" + type + "_" + depth + "_" + id);
+    	set("name", name);
+    }
+    
+	public DimensionType getType() {
+		return this.type;
+	}
+
+	public void setType(DimensionType type) {
+		this.type = type;
+	}
+
+	public int getDepth() {
+		return depth;
+	}
+
+	public void setDepth(int depth) {
+		this.depth = depth;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+}
