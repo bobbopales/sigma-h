ff8e7f3017ef013996c63823f71d2caa462743b1
jbguerline <jbguerline@46f870f5-83fa-7f44-208d-45d3d0f2e120>
2013-07-26 14:10:05 +0000

    Tag for version 1.2.

---
+++ tags/1.2/sigmah/src/main/java/org/sigmah/shared/command/Delete.java
@@ -0,0 +1,74 @@
+/*
+ * All Sigmah code is released under the GNU General Public License v3
+ * See COPYRIGHT.txt and LICENSE.txt.
+ */
+
+package org.sigmah.shared.command;
+
+import org.sigmah.shared.command.result.VoidResult;
+import org.sigmah.shared.domain.ProjectModelStatus;
+import org.sigmah.shared.dto.EntityDTO;
+
+/**
+ * Deletes a database entity.
+ * 
+ * Note: the specified entity must be <code>Deletable</code>
+ * 
+ * Returns <code>VoidResult</code>
+ * 
+ * @see org.sigmah.shared.domain.Deleteable
+ * 
+ */
+public class Delete implements Command<VoidResult> {
+
+	private static final long serialVersionUID = 3931717368143281150L;
+	private String entityName;
+	private int id;
+
+	private ProjectModelStatus projectModelStatus;
+
+	protected Delete() {
+	}
+
+	public Delete(EntityDTO entity) {
+		this.entityName = entity.getEntityName();
+		this.id = entity.getId();
+	}
+
+	public Delete(String entityName, int id) {
+		this.entityName = entityName;
+		this.id = id;
+	}
+
+	public String getEntityName() {
+		return entityName;
+	}
+
+	public void setEntityName(String entityName) {
+		this.entityName = entityName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the projectModelStatus
+	 */
+	public ProjectModelStatus getProjectModelStatus() {
+		return projectModelStatus;
+	}
+
+	/**
+	 * @param projectModelStatus
+	 *            the projectModelStatus to set
+	 */
+	public void setProjectModelStatus(ProjectModelStatus projectModelStatus) {
+		this.projectModelStatus = projectModelStatus;
+	}
+
+}
