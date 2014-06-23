d18288b24c658f8ed8177ccf802f70fdb0ad6a7b
tom.miette <tom.miette@gmail.com>
2014-06-23 14:44:13 +0000

    Patch by spM : Add an "order" column in project/orgunit model fields table. (fixes issue #478)

--- sigmah/src/main/java/org/sigmah/client/page/admin/model/common/element/AdminFlexibleElementsView.java
+++ sigmah/src/main/java/org/sigmah/client/page/admin/model/common/element/AdminFlexibleElementsView.java
@@ -265,7 +265,9 @@ public class AdminFlexibleElementsView extends View {
 		
         List<ColumnConfig> configs = new ArrayList<ColumnConfig>();  
 		  
-        ColumnConfig column = new ColumnConfig("label",I18N.CONSTANTS.adminFlexibleName(), 250);   
+        ColumnConfig column = null;      
+        
+        column = new ColumnConfig("label",I18N.CONSTANTS.adminFlexibleName(), 250);   
         column.setRenderer(new GridCellRenderer<FlexibleElementDTO>(){
 
 			@Override
@@ -451,6 +453,18 @@ public class AdminFlexibleElementsView extends View {
 				
 			}	    	
 	    }); 
+		configs.add(column);
+		
+        column = new ColumnConfig("order",I18N.CONSTANTS.adminFlexibleOrderInGroup(), 50);   
+		column.setRenderer(new GridCellRenderer<FlexibleElementDTO>(){
+
+			@Override
+			public Object render(FlexibleElementDTO model, String property,
+					ColumnData config, int rowIndex, int colIndex,
+					ListStore<FlexibleElementDTO> store, Grid<FlexibleElementDTO> grid) {	
+				return AdminUtil.createGridText(model.getConstraint().getSortOrder()+"");
+			}	    	
+	    });
 		configs.add(column);
 		
 		ColumnModel cm = new ColumnModel(configs);		
