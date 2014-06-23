0593aeb05a9dce88d94f5f1d23bb2a90f0e591d0
tom.miette <tom.miette@gmail.com>
2014-06-23 16:15:13 +0000

    Patch by somzzz : Activate this phase should not disable the Save button. (fixes issue #586)

--- sigmah/src/main/java/org/sigmah/client/page/project/dashboard/ProjectDashboardPresenter.java
+++ sigmah/src/main/java/org/sigmah/client/page/project/dashboard/ProjectDashboardPresenter.java
@@ -887,8 +887,12 @@ public class ProjectDashboardPresenter implements SubPresenter {
 		// --
 
 		// Disabled until a field is modified.
-		view.getButtonSavePhase().setEnabled(false);
-		view.getButtonSavePhase().removeAllListeners();
+		if(hasValueChanged()) {
+			view.getButtonSavePhase().setEnabled(true);
+		} else {
+			view.getButtonSavePhase().setEnabled(false);
+			view.getButtonSavePhase().removeAllListeners();
+		}	
 
 		// If the phase isn't ended, adds the save action.
 		if (!isEndedPhase(projectPresenter.getCurrentDisplayedPhaseDTO())) {
