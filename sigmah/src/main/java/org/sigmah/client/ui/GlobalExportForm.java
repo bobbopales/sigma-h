f80bdfcba318353ae2b4f09b47d9c5afa301f958
tom.miette <tom.miette@gmail.com>
2014-06-24 08:48:23 +0000

    Patch by jason90 : Disable step 2 until auto-export lookup step 1 isn't complete. (fixes issue #577)

--- sigmah/src/main/java/org/sigmah/client/ui/GlobalExportForm.java
+++ sigmah/src/main/java/org/sigmah/client/ui/GlobalExportForm.java
@@ -121,6 +121,7 @@ public class GlobalExportForm {
 				periods.setEditable(false);
 				periods.setTriggerAction(TriggerAction.ALL);
 				periods.setHideLabel(false);
+				periods.setEnabled(false);
 
 				final FlexTable periodTable = new FlexTable();
 				periodTable.setHTML(0, 0, "<b>" + I18N.CONSTANTS.exportBackSelection() + "</b>");
@@ -181,6 +182,7 @@ public class GlobalExportForm {
 										                }
 										                periodsStore.add(result.getList());
 										                periodsStore.commitChanges();
+										                periods.setEnabled(true);
 									                }
 								                }
 							                });
